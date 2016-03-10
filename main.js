d5e74de091d3fcbe18a1dfc0a9ebb5ef7d6bae19
Robyn Ranford <robyn.ranford@gmail.com>
2016-03-10 22:05:30 +0000

    Flashed out main runner

--- main.js
+++ main.js
@@ -1,6 +1,39 @@
 var Components = require('./components');
+var readline = require("readline");
+
+reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
 var inout = new Components.Inout();
 console.log(inout.startMessage());
-inout.promptUser();
+
+
+var calc = new Components.Calculator();
+
+userPrompt = function(){
+	reader.question("Please provide a number?", function(providedNumber){
+		if(isNaN(providedNumber)){
+			tryagain(userPrompt);
+		}
+		else {
+			var processed = calc.process(providedNumber);
+			console.log(processed);
+			userPrompt();
+		}
+	});
+
+};
+
+tryagain = function(callback){
+	console.log('Error that was not a number, please try agian');
+	callback();
+};
+
+userPrompt();
+
+
+
+
 
